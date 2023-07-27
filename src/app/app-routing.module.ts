34172bd80e13a1f8ec5d714afd8c1c3e3dabc8ef
Victor Jerrysson <victorjerrysson@gmail.com>
2023-07-27 16:52:22 -0300

    feat: Adding the links to the sidenav

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,18 +9,22 @@ import { HistoryComponent } from 'src/pages/history/history.component';
 const routes: Routes = [
   {
     path: '',
+    title: 'Início | Conversor de moedas',
     component: HomeComponent,
   },
   {
     path: 'coins',
+    title: 'Listagem | Conversor de moedas',
     component: CoinsListComponent,
   },
   {
     path: 'converter',
+    title: 'Conversor | Conversor de moedas',
     component: ConverterComponent,
   },
   {
     path: 'history',
+    title: 'Histórico | Conversor de moedas',
     component: HistoryComponent,
   },
 ];
